7cf9b839c53dce5d3dd72f6eb2ea919374a0fd64
JinMin KIm <isugar150@gmail.com>
2024-07-25 15:01:17 +0900

    Update AppDelegate+FirebaseMessagingPlugin.h

--- src/ios/AppDelegate+FirebaseMessagingPlugin.h
+++ src/ios/AppDelegate+FirebaseMessagingPlugin.h
@@ -1,8 +1,19 @@
+#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 @import UserNotifications;
 @import FirebaseMessaging;
 
-@interface AppDelegate (FirebaseMessagingPlugin) <FIRMessagingDelegate, UNUserNotificationCenterDelegate>
+//@interface AppDelegate (FirebaseMessagingPlugin) <FIRMessagingDelegate, UNUserNotificationCenterDelegate>
+//
+//@end
+
+
+@interface AppDelegate (FirebaseMessagingPlugin)
+@property (nonatomic, strong) NSDictionary *pendingNotification;
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
+- (void)messaging:(FIRMessaging *)messaging didReceiveRegistrationToken:(NSString *)fcmToken;
+- (void)applicationDidBecomeActive:(UIApplication *)application;
 
 @end
