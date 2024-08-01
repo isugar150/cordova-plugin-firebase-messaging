aa155a01d70aa59610d633810e15e40bb89d377c
JinMin KIm <isugar150@gmail.com>
2024-08-01 10:41:52 +0900

    Update MainActivity.java

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -6,9 +6,7 @@
        to you under the Apache License, Version 2.0 (the
        "License"); you may not use this file except in compliance
        with the License.  You may obtain a copy of the License at
-
          http://www.apache.org/licenses/LICENSE-2.0
-
        Unless required by applicable law or agreed to in writing,
        software distributed under the License is distributed on an
        "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,9 +14,7 @@
        specific language governing permissions and limitations
        under the License.
  */
-
-package ai.riskzero.sisul3.manager;
-
+package ai.riskzero.zg.manager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -26,29 +22,25 @@ import org.apache.cordova.*;
 import android.content.Intent;
 import by.chemerisuk.cordova.firebase.FirebaseMessagingPluginService;
 import android.util.Log;
-
 public class MainActivity extends CordovaActivity
 {
     private static final String TAG = "MainActivity";
     private String pendingNavigationUrl = "http://localhost/#";
-
+    private boolean restart = false;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate called");
-
         // enable Cordova apps to be started in the background
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
-
         // Set by <content src="index.html" /> in config.xml
 //        loadUrl(launchUrl);
         handleIntent(getIntent());
     }
-
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
@@ -56,7 +48,6 @@ public class MainActivity extends CordovaActivity
 //        setIntent(intent);
         handleIntent(intent);
     }
-
     private void handleIntent(Intent intent) {
         if (intent.getExtras() != null) {
             for (String key : intent.getExtras().keySet()) {
@@ -75,10 +66,21 @@ public class MainActivity extends CordovaActivity
             String url = intent.getExtras().get("navigateTo").toString();
             Log.d("MainActivity.url: ", url);
             if (url != null && !url.isEmpty()) {
-                 loadUrl("http://localhost/#" + url);
+                 if(restart){
+                    loadUrl("javascript:setTimeout(function() { window.location.hash = '"+url+"'; }, 100);");
+                 } else {
+                    loadUrl("http://localhost/#" + url);
+                 }
             }
         } else {
             loadUrl(launchUrl);
         }
+    }
+     @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+//        setIntent(intent);
+        restart = true;
     }
 }
